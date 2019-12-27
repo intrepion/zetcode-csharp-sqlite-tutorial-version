beb319d3ae5be94aae2c1254835136da72e44562
Oliver Forral <intrepion@gmail.com>
2019-12-26 20:43:16 -0800

    dotnet new console --name Version

---
+++ Version/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
